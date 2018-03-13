57da67e19570d3c6ac8915de670fa8050b8efb8e
m-rodriguez7 <michael.rodriguez7@icloud.com>
2018-03-13 01:37:05 -0700

    game functions/displays set, need to add style, game initiation, game reset, win count? loss count? etc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
+// variables to be used in code
+
 var wins = 0;
 var words = ['Awkward', 'Bagpipes', 'Banjo', 'Bungler', 'Croquet', 'Crypt', 'Dwarves', 'Fervid', 'Fishhook', 'Fjord', 'Gazebo', 
 'Gypsy', 'Haiku', 'Haphazard', 'Hyphen', 'Ivory', 'Jazzy', 'Jiffy', 'Jinx', 'Jukebox', 'Kayak', 'Kiosk', 'Klutz', 'Memento', 'Mystify', 
 'Numbskull', 'Ostracize', 'Oxygen', 'Pajama', 'Phlegm', 'Pixel', 'Polka', 'Quad', 'Quip', 'Rhythmic', 'Rogue', 'Sphinx', 'Squawk', 
 'Swivel', 'Toady', 'Twelfth', 'Waxy', 'Wildebeest', 'Yacht', 'Zealous', 'Zigzag', 'Zippy', 'Zombie'];
 // var currentWord;
+var nonAlphabet = ["control", "shift", "meta", "alt", "enter", "backspace", "escape", "insert", "home", "pageup", "pagedown", "end", "delete", 
+"arrowup", "arrowdown", "arrowleft", "arrowright", "print", "scrolllock", "pause"] // used dev tools to create this list
 var hiddenWord = [];
 var triesLeft = 12;
 var guesses = [];
-
 // I'm too lazy to go through the array and lower case everything so there ya go
 for (var i=0; i < words.length; i++) {
     words[i] = words[i].toLowerCase();
@@ -15,11 +18,8 @@ for (var i=0; i < words.length; i++) {
 
 
 
-// NEED TO FIGURE OUT HOW TO DISPLAY JAVASCRIPT STUFF ON THE WEBPAGE (HTML)
-
-
-
 // game functions here
+
 var addTo = function (array, letter) {
     if (array.includes(letter)) {
         return;
@@ -28,7 +28,6 @@ var addTo = function (array, letter) {
         console.log(guesses);
     }
 }
-
 var makeDash = function(word) {
     var dash = [];
     for (var i = 0; i < word.length; i++) {
@@ -37,23 +36,20 @@ var makeDash = function(word) {
     // return dash.join(""); THIS CONVERTS THE ARRAY TO A STRING *ANGRY*
     return dash; // return array for further modification, use .join outside of function for display purposes only
 }
-
 var revealIf = function(key, word, dash) { // would be using event.key in place of "key"
     for (var i=0; i < word.length; i++) {// "reveal if" key is in the word and replace its dash.
         if (key === word[i]) {
             console.log("key match!");
             dash[i] = key;
-            document.getElementById("randomWord").textContent = dash.join("");
+            document.getElementById("randomWord").textContent = dash.join(" ");
         } 
     console.log("done looking");
     }
 }
 
-// use .push to add to arrays!
-// would need to display all elements before a button press, but use the button press to modify them.
-    // i will leave guesses in the button press code, so it will only show when something has been added to it.     
 
 // variables made with functions here
+
 var guessThis = words[Math.floor(Math.random() * words.length)]; // Math... gives out random whole number between 0 and the length of words used for the hangman game
 var guessDash = makeDash(guessThis); // this creates an ARRAY
 console.log(guessThis + " is the current word");
@@ -63,7 +59,6 @@ console.log(guessThis + " is the current word");
 
 // game display here
 
-
 document.getElementById("randomWord").textContent = makeDash(guessThis).join("");
 document.getElementById("tries").textContent = triesLeft;
 document.getElementById("guess").textContent = guesses;
@@ -71,9 +66,16 @@ document.getElementById("guess").textContent = guesses;
 
 
 // game functions here
-document.onkeyup = function(event) {
+
+document.onkeyup = function(event) { // toLowerCase() used throughout to check for case-sensitivity
+    if (!/^[a-zA-Z]+$/.test(event.key)) {
+        console.log("you pressed a non-alphabetical key!");
+        return;
+    } else if (nonAlphabet.includes(event.key.toLowerCase())) { // previous conditional still allowed system/function keys
+        console.log("you pressed a non-alphabetical key!"); 
+        return;
+    } else {
     revealIf(event.key.toLowerCase(), guessThis, guessDash);
-    
     if (guesses.includes(event.key.toLowerCase()) || guessDash.includes(event.key.toLowerCase())) {
         console.log("should NOT subtract");
     } else { 
@@ -82,5 +84,6 @@ document.onkeyup = function(event) {
         triesLeft--;
         document.getElementById("tries").textContent = triesLeft; // need to update triesLeft in the html, or the display on the page never changes
     }
-    document.getElementById("guess").textContent = guesses;
+    document.getElementById("guess").textContent = guesses.join(" ");
+    }
 }
